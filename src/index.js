dc5dd11d20f03fd451d7885d5208412ab1a1eefc
Tyge <tygesjohnson@gmail.com>
2018-02-05 15:50:09 -0700

    index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM, { Render } from 'react-dom';
 
 import App from './components/App';
+import './index.css';
+
+const movies = [
+ {
+   id: 1,
+   title: 'The Lord of The Rings',
+ },
+ {
+   id: 2,
+   title: 'The Hobbit',
+ },
+ {
+   id: 3,
+   title: 'Sta Wars',
+ },
+];
+
+function Movie(props) {
+  return <li>{prop.movie.title}</li>
+}
+
+class MoviesList extends React.Component {
+  render() {
+    return (
+      <div className="movei-list">
+        <h1>movie list component</h1>
+        <ul>
+          {this.props.films.map(movie => {
+            return <Movie key={movie.id} movie={movie} />;
+          })}
+        </ul>
+
+      </div>
+    );
+  }
+}
 
 // function Movie () {
 //   return <div> movie component</div>;
@@ -15,5 +51,5 @@ import App from './components/App';
 //     );
 //   }
 // }
-
+ ReactDOM.render(<MoviesList films={movies} />, document.getElementById('root'));
 render(<App />, document.getElementById('root'));
