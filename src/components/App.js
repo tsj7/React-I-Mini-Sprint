9700d79aefcc039d287d2302bd4d15adfeee0507
Tyge <tygesjohnson@gmail.com>
2018-02-06 10:36:07 -0700

    components

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,49 @@
-import React, { Component } from 'react';
-import Render from 'react-dom';
-import MoviesList from './MoviesList';
+import React from 'react';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: ['movie1', 'movie2', 'movie3'],
-    };
-  }
-  render() {
-    console.log("movies");
-    return (
-      <div>
-        <MoviesList movies={this.state.movies} />
-      </div>
-    )
-  }
+function Movie(props) {
+  return <li>{props.movie.title}</li>;
 }
 
-export default App;
+export default Movie;
 
-
-
-/////////
-
-// Step 1: Import React, and Component from react.
-// Step 2: Import render from react-dom
-// Step 3: Import MoviesList from MoviesList
-
-// Step 4: Write a class called App, extending Component.
-// Step 5: Write a constructor function without passing anything into it.
-// Step 6: Inside the constructor function, call super without passing anything into it.
-// Step 7: Inside the constructor function, create a state on this, and set it to an object.
-// Step 8: Inside the object, set a key to movies, and the value to an array of movies
-// Step 9: Write a render function.
-// Step 10: Inside the render function, write a return statement.
-// Step 11: Inside the return statement, write a selfclosing tag called MoviesList
-// Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
-// Step 13: Outside the class, export the App class as a default.
+// import React, { Component } from 'react';
+// import Render from 'react-dom';
+// import MoviesList from './MoviesList';
+//
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       movies: ['movie1', 'movie2', 'movie3'],
+//     };
+//   }
+//   render() {
+//     console.log("movies");
+//     return (
+//       <div>
+//         <MoviesList movies={this.state.movies} />
+//       </div>
+//     )
+//   }
+// }
+//
+// export default App;
+//
+//
+//
+// /////////
+//
+// // Step 1: Import React, and Component from react.
+// // Step 2: Import render from react-dom
+// // Step 3: Import MoviesList from MoviesList
+//
+// // Step 4: Write a class called App, extending Component.
+// // Step 5: Write a constructor function without passing anything into it.
+// // Step 6: Inside the constructor function, call super without passing anything into it.
+// // Step 7: Inside the constructor function, create a state on this, and set it to an object.
+// // Step 8: Inside the object, set a key to movies, and the value to an array of movies
+// // Step 9: Write a render function.
+// // Step 10: Inside the render function, write a return statement.
+// // Step 11: Inside the return statement, write a selfclosing tag called MoviesList
+// // Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
+// // Step 13: Outside the class, export the App class as a default.
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,20 +1,67 @@
 import React from 'react';
 
-const MoviesList = props => {
-  return (
-    <div>
-      <input type="text" value={props.title} onChange={props.changeHandler} />
-      <button onClick={props.addToMovies}>Submit Movie</button>
-      <ul>
-        {props.movies.map((movie, i) => {
-          return <li key={i + 1}>{movie}</li>;
-        })}
-      </ul>
-    </div>
-  );
-};
+import Movie from './Movie';
 
-export default MoviesList;
+class MovieList extends React.Component {
+  state = {
+    movies: [
+      {
+        id: 1,
+        title: 'The Lord of The Rings',
+      },
+      {
+        id: 2,
+        title: 'The Hobbit',
+      },
+      {
+        id: 3,
+        title: 'Sta Wars',
+      },
+    ],
+    message: 'old message',
+  };
+
+  changeMessage = () => {
+    this.setState({ message: 'new Message' });
+  };
+
+  render() {
+    return (
+      <div className="movie-list">
+        <button onClick={this.changeMessage}>Change Message</button>
+        <h1>{this.props.heading}</h1>
+        <h2>{this.state.message}</h2>
+        <ul>
+          {this.state.movies.map(movie => {
+            return <Movie key={movie.id} movie={movie} />;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
+
+
+//// My first take below, Luis's solution above
+// import React from 'react';
+//
+// const MoviesList = props => {
+//   return (
+//     <div>
+//       <input type="text" value={props.title} onChange={props.changeHandler} />
+//       <button onClick={props.addToMovies}>Submit Movie</button>
+//       <ul>
+//         {props.movies.map((movie, i) => {
+//           return <li key={i + 1}>{movie}</li>;
+//         })}
+//       </ul>
+//     </div>
+//   );
+// };
+//
+// export default MoviesList;
 
 // Import React from react.
 
