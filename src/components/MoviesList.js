b9e4426e017ca67bbfc407916f976e216a7a3c52
Tyge <tygesjohnson@gmail.com>
2018-02-05 17:06:24 -0700

    MoviesList.js functional

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,3 +1,21 @@
+import React from 'react';
+
+const MoviesList = props => {
+  return (
+    <div>
+      <input type="text" value={props.title} onChange={props.changeHandler} />
+      <button onClick={props.addToMovies}>Submit Movie</button>
+      <ul>
+        {props.movies.map((movie, i) => {
+          return <li key={i + 1}>{movie}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default MoviesList;
+
 // Import React from react.
 
 // Create a const arrow function called MoviesList.
